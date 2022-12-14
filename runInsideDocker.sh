118f62e8184d75f4b97359019795559f3b23b363
lsu <lsu@17b592ea2961>
2022-12-14 14:28:05 +0000

    change export script from docker

--- runInsideDocker.sh
+++ runInsideDocker.sh
@@ -29,7 +29,7 @@ exportForDataDivider(){
 exportRecords(){
 	local dataDivider=$1
 	local file="/"$baseDir"/"$dataDivider"/1_records.sql"
-	local sql="select * from record where datadivider = '"$dataDivider"'"
+	local sql="select * from record where datadivider = '"$dataDivider"' order by type, id"
 
 	echo "COPY public.record (type, id, datadivider, data) FROM stdin;" > $file
 	runExport "$sql" "$file"
@@ -44,7 +44,7 @@ runExport(){
 exportLinks(){
 	local dataDivider=$1
 	local file="/"$baseDir"/"$dataDivider"/2_links.sql"
-	local sql="select l.* from link l join record r on l.fromtype = r.\"type\"  and l.fromid = r.id where datadivider = '"$dataDivider"'"
+	local sql="select l.* from link l join record r on l.fromtype = r.\"type\"  and l.fromid = r.id where datadivider = '"$dataDivider"' order by fromtype, fromid"
 
 	echo "COPY public.link (fromtype, fromid, totype, toid) FROM stdin;" > $file
 	runExport "$sql" "$file"
@@ -52,7 +52,7 @@ exportLinks(){
 exportStorageTerms(){
 	local dataDivider=$1
 	local file="/"$baseDir"/"$dataDivider"/3_storageterms.sql"
-	local sql="select recordtype, recordid, storagetermid, value, storagekey from storageterm s join record r on s.recordtype = r."type"  and s.recordid = r.id where datadivider = '"$dataDivider"'"
+	local sql="select recordtype, recordid, storagetermid, value, storagekey from storageterm s join record r on s.recordtype = r."type"  and s.recordid = r.id where datadivider = '"$dataDivider"' order by recordtype, recordid"
 
 	echo "COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;" > $file
 	runExport "$sql" "$file"
