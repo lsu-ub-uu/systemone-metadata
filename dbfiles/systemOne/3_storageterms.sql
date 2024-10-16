26923373618172514e8892c1906c7c7c398586e4
DiVA Support <diva-support@ub.uu.se>
2024-10-16 10:54:44 +0200

    DbFiles updated Wed Oct 16 10:54:44 AM CEST 2024

--- dbfiles/systemOne/3_storageterms.sql
+++ dbfiles/systemOne/3_storageterms.sql
@@ -1,13 +1,25 @@
 COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey) FROM stdin;
+user	coraUser:15697099230192	userLoginIdStorageTerm	olfel499@user.uu.se	loginId
 user	coraUser:15697099230192	userUserIdStorageTerm	olfel499@user.uu.se	userId
 user	coraUser:196015452870546	userUserIdStorageTerm	perba577@user.uu.se	userId
+user	coraUser:196015452870546	userLoginIdStorageTerm	perba577@user.uu.se	loginId
+user	coraUser:1986901372214875	userLoginIdStorageTerm	kenan399@user.uu.se	loginId
 user	coraUser:1986901372214875	userUserIdStorageTerm	kenan399@user.uu.se	userId
 user	coraUser:8012331835977	userUserIdStorageTerm	mican434@user.uu.se	userId
+user	coraUser:8012331835977	userLoginIdStorageTerm	mican434@user.uu.se	loginId
+user	coraUser:8239436126036	userLoginIdStorageTerm	stand516@user.uu.se	loginId
 user	coraUser:8239436126036	userUserIdStorageTerm	stand516@user.uu.se	userId
+user	user:11385927333713116	userLoginIdStorageTerm	palra847@user.uu.se	loginId
 user	user:11385927333713116	userUserIdStorageTerm	palra847@user.uu.se	userId
 user	user:11385961546081631	userUserIdStorageTerm	andma210@user.uu.se	userId
+user	user:11385961546081631	userLoginIdStorageTerm	andma210@user.uu.se	loginId
 user	user:13641514994783267	userUserIdStorageTerm	marvi707@user.uu.se	userId
+user	user:13641514994783267	userLoginIdStorageTerm	marvi707@user.uu.se	loginId
 user	user:13641636507033579	userUserIdStorageTerm	evaag112@user.uu.se	userId
+user	user:13641636507033579	userLoginIdStorageTerm	evaag112@user.uu.se	loginId
 user	user:13641673157161404	userUserIdStorageTerm	sarto903@user.uu.se	userId
+user	user:13641673157161404	userLoginIdStorageTerm	sarto903@user.uu.se	loginId
+user	user:13641982216972183	userLoginIdStorageTerm	egisw290@user.uu.se	loginId
 user	user:13641982216972183	userUserIdStorageTerm	egisw290@user.uu.se	userId
+user	user:13642019143826316	userLoginIdStorageTerm	danfl648@user.uu.se	loginId
 user	user:13642019143826316	userUserIdStorageTerm	danfl648@user.uu.se	userId
