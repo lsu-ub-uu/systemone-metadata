d869574f72101fcdf13b6432d86876ac58b718df
DiVA Support <diva-support@ub.uu.se>
2024-12-13 07:55:32 +0100

    DbFiles updated Fri Dec 13 07:55:32 AM CET 2024

--- dbfiles/systemOne/3_storageterms.sql
+++ dbfiles/systemOne/3_storageterms.sql
@@ -2,7 +2,6 @@ COPY public.storageterm (recordtype, recordid, storagetermid, value, storagekey)
 user	coraUser:15697099230192	userLoginIdStorageTerm	olfel499@user.uu.se	loginId
 user	coraUser:196015452870546	userLoginIdStorageTerm	perba577@user.uu.se	loginId
 user	coraUser:1986901372214875	userLoginIdStorageTerm	kenan399@user.uu.se	loginId
-user	coraUser:8012331835977	userLoginIdStorageTerm	mican434@user.uu.se	loginId
 user	coraUser:8239436126036	userLoginIdStorageTerm	stand516@user.uu.se	loginId
 user	user:11385927333713116	userLoginIdStorageTerm	palra847@user.uu.se	loginId
 user	user:11385961546081631	userLoginIdStorageTerm	andma210@user.uu.se	loginId
@@ -10,4 +9,3 @@ user	user:13641514994783267	userLoginIdStorageTerm	marvi707@user.uu.se	loginId
 user	user:13641636507033579	userLoginIdStorageTerm	evaag112@user.uu.se	loginId
 user	user:13641673157161404	userLoginIdStorageTerm	sarto903@user.uu.se	loginId
 user	user:13641982216972183	userLoginIdStorageTerm	egisw290@user.uu.se	loginId
-user	user:13642019143826316	userLoginIdStorageTerm	danfl648@user.uu.se	loginId
